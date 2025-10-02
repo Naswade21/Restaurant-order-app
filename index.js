2441bbbeaef522f52d54ec0ca344a9bcccf0eb04
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2025-10-02 14:20:28 -0400

    added in rendered menu-items into project

--- index.js
+++ index.js
@@ -3,13 +3,46 @@ import { menuArray } from '/data.js'
 //object destructuring
 const { name, ingredients, id, price, emoji } = menuArray
 
+/* Render the menu-items
+     <div class="menu-wrap">
+                    <div class="menu-content">
+                        <div class="menu-emoji">🍕<!-- Insert emoji --></div>
+                        <div class="menu-desc">
+                            <!-- Insert desc -->
+                        </div>
+                    </div>
+                    <div class="addItem">
+                        <button>+</button>
+                    </div>
+                 </div>
+*/
+
 const menu = document.getElementById('menu')
 const checkOut = document.getElementById('checkout')
 
 const getMenuHtml = () => {
     return menuArray.map((menu) => {
-        return ``
-    })
+        return `<div class="menu-wrap">
+                    <div class="menu-content">
+                        <div class="menu-emoji">${menu.emoji}<!-- Insert emoji --></div>
+                        <div class="menu-desc">
+                        <p>${menu.name}</p>
+                        <p>${menu.ingredients}</p>
+                        <p>${menu.price}</p>
+                            <!-- Insert desc -->
+                        </div>
+                    </div>
+                    <div class="addItem">
+                        <button id="${menu.id}">+</button>
+                    </div>
+                 </div>`
+    }).join('')
 }
 
-console.log(getMenuHtml())
+const render = () => {
+    return menu.innerHTML = getMenuHtml()
+}
+
+render()
+
+//console.log(getMenuHtml())
